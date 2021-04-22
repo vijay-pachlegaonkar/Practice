bf5538b007e54446f41b5a7c5a0fff2d1265d923
vijay-pachlegaonkar <61426577+vijay-pachlegaonkar@users.noreply.github.com>
2021-04-22 18:06:33 +0530

    modified the name

--- HelloWorld.java
+++ HelloWorld.java
@@ -5,6 +5,6 @@ class HelloWorld {
         System.out.println("Hello, World!"); 
         System.out.println("This is the first java program.");
         System.out.println("This is the very simple java program."); 
-        System.out.println("Vijay"); 
+        System.out.println("Vijay-P"); 
     }
 }
