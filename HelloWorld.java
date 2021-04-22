210fc1a18ec4d550a8805a7d3b2a4aba26e0cc13
Vijay Pachlegaonkar <vijaypachlegaonkar@gmail.com>
2021-04-22 23:13:21 +0530

    deleted 2nd line

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,6 +3,5 @@
 class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, World!"); 
-        System.out.println("This is the first java program.");
     }
 }
